1a147a986dc2fa3ab4b1194568394613c8ac16bc
Austin Tung <austin.tung56@gmail.com>
2020-03-20 17:23:16 -0700

    Updates

---
+++ precache-manifest.6a7edbb3c21e430cb1b4ef86cfd3d442.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e710f447532532f8b4e1c9b33c4acc23",
+    "url": "/SoundBoard/index.html"
+  },
+  {
+    "revision": "711a1239dd48d58d71bb",
+    "url": "/SoundBoard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "31e2e3647b2dfe848e73",
+    "url": "/SoundBoard/static/js/2.48dc1793.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SoundBoard/static/js/2.48dc1793.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "711a1239dd48d58d71bb",
+    "url": "/SoundBoard/static/js/main.6faad094.chunk.js"
+  },
+  {
+    "revision": "43c28bcb67e4bfd7b067",
+    "url": "/SoundBoard/static/js/runtime-main.b61953d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SoundBoard/static/media/logo.5d5d9eef.svg"
+  }
+]);
